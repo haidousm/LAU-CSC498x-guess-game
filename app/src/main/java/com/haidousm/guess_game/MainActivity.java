21207b07c4ae7514dcda7afc63c4c28b938a51cb
haidousm <haidous.m@gmail.com>
2021-10-22 12:43:15 +0300

    display image + button answers

--- app/src/main/java/com/haidousm/guess_game/MainActivity.java
+++ app/src/main/java/com/haidousm/guess_game/MainActivity.java
@@ -2,23 +2,56 @@ package com.haidousm.guess_game;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Map<String, String> titleImgMap;
+    private ArrayList<String> titles;
+    private ArrayList<String> imgUrls;
+
+    private int currentAppIndex = 0;
+
+    private DownloadImageTask downloadImageTask;
+
+    private ImageView appIconImageView;
+    private Button answerAButton;
+    private Button answerBButton;
+    private Button answerCButton;
+    private Button answerDButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.appIconImageView = findViewById(R.id.appIconImageView);
+        this.answerAButton = findViewById(R.id.answerAButton);
+        this.answerBButton = findViewById(R.id.answerBButton);
+        this.answerCButton = findViewById(R.id.answerCButton);
+        this.answerDButton = findViewById(R.id.answerDButton);
+
+        this.downloadImageTask = new DownloadImageTask(appIconImageView);
 
         String listSiteURL = "https://www.pcmag.com/picks/best-android-apps";
 
@@ -27,9 +60,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void finishedLoadingData(Map<String, String> titleImgMap) {
+    public void finishedLoadingData(Map<String, String> titleImgMap, ArrayList<String> titles, ArrayList<String> imgUrls) throws IOException {
 
-        Log.i("guess-game", String.valueOf(titleImgMap.size()));
+        this.titleImgMap = titleImgMap;
+        this.titles = titles;
+        this.imgUrls = imgUrls;
+        Collections.shuffle(titles);
+
+        startLoadingNextApp();
+    }
+
+    public void startLoadingNextApp() {
+        String correctTitle = titles.get(currentAppIndex);
+        String imgUrl = titleImgMap.get(correctTitle);
+        this.downloadImageTask.execute(imgUrl);
+    }
+
+    public void finishedLoadingNextApp() {
+
+        Random random = new Random();
+        String[] chosenTitles = new String[]{
+                this.titles.get(currentAppIndex),
+                this.titles.get(random.nextInt(this.titles.size())),
+                this.titles.get(random.nextInt(this.titles.size())),
+                this.titles.get(random.nextInt(this.titles.size()))};
+
+        this.answerAButton.setText(chosenTitles[0]);
+        this.answerBButton.setText(chosenTitles[1]);
+        this.answerCButton.setText(chosenTitles[2]);
+        this.answerDButton.setText(chosenTitles[3]);
+
+        currentAppIndex++;
 
     }
 
@@ -71,6 +132,9 @@ public class MainActivity extends AppCompatActivity {
 
                 Map<String, String> titleImgMap = new HashMap<>();
 
+                ArrayList<String> titles = new ArrayList<>();
+                ArrayList<String> imgUrls = new ArrayList<>();
+
                 for (int i = 1; i < cardDivs.length; i++) {
 
                     String div = cardDivs[i];
@@ -79,6 +143,9 @@ public class MainActivity extends AppCompatActivity {
 
                     String imageUrl = div.substring(leftPointer, rightPointer);
 
+                    int lastSlashIndex = imageUrl.lastIndexOf("/");
+                    imageUrl = imageUrl.substring(0, lastSlashIndex) + imageUrl.substring(lastSlashIndex, imageUrl.indexOf(".", lastSlashIndex)) + imageUrl.substring(imageUrl.lastIndexOf("."));
+
                     leftPointer = div.indexOf(titleMatcher) + titleMatcherLength;
                     rightPointer = div.indexOf("</h2>", leftPointer);
 
@@ -86,15 +153,44 @@ public class MainActivity extends AppCompatActivity {
 
                     titleImgMap.put(titleUrl, imageUrl);
 
+                    titles.add(titleUrl);
+                    imgUrls.add(imageUrl);
+
                 }
 
-                MainActivity.this.finishedLoadingData(titleImgMap);
+                MainActivity.this.finishedLoadingData(titleImgMap, titles, imgUrls);
 
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
 
+        }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView bmImage;
+
+        public DownloadImageTask(ImageView bmImage) {
+            this.bmImage = bmImage;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String urldisplay = urls[0];
+            Bitmap mIcon11 = null;
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+            return mIcon11;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            bmImage.setImageBitmap(result);
+            MainActivity.this.finishedLoadingNextApp();
         }
     }
 }
